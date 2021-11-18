a0584166d5899c86b1922a110eea6910c2c6f3b5
vinicius-hora <vinicius_hora@live.com>
2021-11-18 08:25:16 -0300

    inserindo Crosorigin para acesso aos endpoints

--- src/main/java/curso/api/rest/SpringrestapiApplication.java
+++ src/main/java/curso/api/rest/SpringrestapiApplication.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //configurando spring para reconhecer os pacotes
 @SpringBootApplication
@@ -19,10 +21,21 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
-public class SpringrestapiApplication {
+public class SpringrestapiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestapiApplication.class, args);
 	}
+	/*
+	//configuração centralizada para liberação de acessos
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/usuario/**");
+		//liberando metodos especificos para servidores especificos
+		registry.addMapping("/usuario/**").allowedMethods("GET")
+		.allowedOrigins("servidor, servidor2");
+		
+	}
+	*/
 
 }
