5766af92bc8e7a91d95a5744b4e89a96565152b3
vinicius-hora <vinicius_hora@live.com>
2021-11-22 08:51:27 -0300

    tratamento de erros

---
+++ src/main/java/curso/api/rest/error/ControleExcessoes.java
@@ -0,0 +1,73 @@
+package curso.api.rest.error;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+
+@RestControllerAdvice
+@ControllerAdvice
+public class ControleExcessoes extends ResponseEntityExceptionHandler {
+	//interceptar erros mais comuns
+	@ExceptionHandler({Exception.class, RuntimeException.class, Throwable.class})
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		String msg = "";
+		//montando a mensagen
+		if(ex instanceof MethodArgumentNotValidException) {
+			List<ObjectError> list = ((MethodArgumentNotValidException) ex).getBindingResult().getAllErrors();
+			for(ObjectError objectError: list) {
+				msg += objectError.getDefaultMessage() + "\n";
+			}
+			
+		}else {
+			msg = ex.getMessage();
+		}
+		
+		ObjetoErro objetoErro = new ObjetoErro();
+		objetoErro.setError(msg);
+		objetoErro.setCode(status.value() + "==>" + status.getReasonPhrase());
+		
+		return new ResponseEntity<>(objetoErro, headers, status);
+	}
+	// tratamento de erros a nível de banco de dados
+	@ExceptionHandler({DataIntegrityViolationException.class, ConstraintViolationException.class,
+		SQLException.class})
+	protected ResponseEntity<Object> handleExceptionDataIntegry(Exception ex){
+		
+		String msg = "";
+		
+		if(ex instanceof DataIntegrityViolationException) {
+			msg = ((DataIntegrityViolationException) ex).getCause().getCause().getMessage();
+		}else if(ex instanceof ConstraintViolationException){
+			msg = ((ConstraintViolationException) ex).getCause().getCause().getMessage();
+			
+		}else if(ex instanceof SQLException){
+		msg = ((SQLException) ex).getCause().getCause().getMessage();
+		
+		}else {
+			msg = ex.getMessage();
+		}
+		
+		ObjetoErro objectError = new ObjetoErro();
+		objectError.setError(msg);
+		objectError.setCode(HttpStatus.INTERNAL_SERVER_ERROR + "==>"+ HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+		
+		return new ResponseEntity<>(objectError, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
